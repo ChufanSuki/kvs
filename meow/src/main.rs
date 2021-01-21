0ad0e9e15c883c8a5e276d243ab8d34e7b7fd8bb
Chufan Chen <allenplato28@gmail.com>
2021-01-21 22:07:10 +0800

    Inital Commit. Play with cli program.

---
+++ meow/src/main.rs
@@ -0,0 +1,37 @@
+use clap::{App, load_yaml};
+
+fn main() {
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(i) = matches.value_of("input") {
+        println!("Value for input: {}", i);
+    }
+
+    if let Some(c) = matches.value_of("config") {
+        println!("Value for config: {}", c);
+    }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match matches.occurrences_of("v") {
+        0 => println!("Verbose mode is off"),
+        1 => println!("Verbose mode is kind of on"),
+        2 => println!("Verbose mode is on"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level app
+    if let Some(ref matches) = matches.subcommand_matches("test") {
+        // "$ myapp test" was run
+        if matches.is_present("debug") {
+            // "$ myapp test -d" was run
+            println!("Printing debug info...");
+        } else {
+            println!("Printing normally...");
+        }
+    }
+    // ...
+}
